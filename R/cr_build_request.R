328a50e314c6cf42294a126d3921543bcb1930c8
Bochtler, Paul <65470117+datapumpernickel@users.noreply.github.com>
2023-12-13 22:53:19 +0100

    added basic building of request

---
+++ R/cr_build_request.R
@@ -0,0 +1,37 @@
+#' Build a valid request object from the checked parameters
+#'
+#' @returns a httr2 request object
+#' @inheritParams cr_get_data
+cr_build_request <- function(params,
+                             verbose = FALSE) {
+
+  params_full <- c()
+  for(name in names(params)){
+    values <- params[[name]]
+    for(value in values){
+      if(!is.null(value)){
+        temp <- value
+        names(temp) <- name
+      }
+      params_full <- c(params_full, temp)
+    }
+  }
+
+
+
+  res <-
+    httr2::request("https://ec.europa.eu/eurostat/api/comext/dissemination/statistics/1.0/data/ds-059322/") |>
+    httr2::req_url_query(!!!params_full)
+
+  if(stringr::str_length(res$url)>4095){
+    rlang::abort("Your request exceeds 4KB or 4096 characters, which is the upper limit of the Comtrade API.") # nolint
+  }
+
+  if (verbose) {
+    cli::cli_inform(c("i" = paste0("URL that will be queried: ",res$url)))
+  }
+
+  return(res)
+}
+
+
