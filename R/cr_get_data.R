ec7cdc9808338e928edc747e6c1377aab06469a7
Bochtler, Paul <65470117+datapumpernickel@users.noreply.github.com>
2023-12-13 20:11:15 +0100

    added skeleton of get data function

---
+++ R/cr_get_data.R
@@ -0,0 +1,51 @@
+#' Get trade data from the Comex API
+#'
+#' This function will get Comex data from the new API.
+#'
+#' @details
+#'
+#' @export
+#' @returns A data.frame with trade data or, if `process = F`, a httr2 response object.
+
+ct_get_data <- function(freq,
+                        reporter,
+                        partner,
+                        stat_procedure,
+                        product,
+                        time,
+                        indicators,
+                        flow,
+                        process = TRUE,
+                        tidy_cols = TRUE,
+                        verbose = FALSE,
+                        update = T,
+                        requests_per_second = 10 / 60) {
+  ## compile codes
+  params <- cr_check_params(
+    freq = freq,
+    reporter = reporter,
+    partner = partner,
+    stat_procedure = stat_procedure,
+    product = product,
+    time = time,
+    indicators = indicators,
+    flow = flow,
+    update = update
+    )
+
+  req <-
+    ct_build_request(params, verbose = verbose,
+                     primary_token = primary_token)
+
+  resp <- ct_perform_request(req,
+                             requests_per_second = requests_per_second,
+                             verbose = verbose)
+
+  if (process) {
+    result <- ct_process_response(resp, verbose = verbose,
+                                  tidy_cols = tidy_cols)
+    return(result)
+  } else{
+    return(resp)
+  }
+}
